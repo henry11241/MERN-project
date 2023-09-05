d6cd897765228fd14c3fe73b13eb0eb47317f0c4
Henry Lu <henry11241@gmail.com>
2023-09-05 16:26:13 +0800

    Handle error exception

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-  res.status(200).json({message: 'Welcome to the Support Dest API'})
+  res.status(200).json({ message: 'Welcome to the Support Dest API' })
 })
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
